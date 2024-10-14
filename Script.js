0cd51673f1f630df0155ed375a18654f41ba773a
Herreinstein Roman <63233856+GRom32Bit@users.noreply.github.com>
2024-10-14 14:16:19 +0500

    ОсноваСайта

---
+++ Script.js
@@ -0,0 +1,5 @@
+window.addEventListener("resize", function () {
+    document.getElementById("151").style.width = calc(window.screen.width * 0.3);
+    document.getElementById("151").style.height = 1.147 * document.getElementById("151").style.width;
+}
+);
