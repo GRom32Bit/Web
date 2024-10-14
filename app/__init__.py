a65eb141bc4ae91e56318fc33ad40005b7c46229
Herreinstein Roman <63233856+GRom32Bit@users.noreply.github.com>
2024-10-14 14:12:40 +0500

    Delete app directory

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask import SQLAlchemy
-
-app = Flask(__name__, template_folder='templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres: MadNess19@localhost/Web'
-db = SQLAlchemy(app)
-
-from app import routes
